6ad71f99db63d0d69b47558095beab909c69672c
Keivan Razban <keivan.razban@gmail.com>
2020-11-10 11:01:25 +0000

    point follows eyes, very very unaccurate

--- cam.py
+++ cam.py
@@ -34,8 +34,11 @@ def draw_eye(X1,X2, gray,img):
     try:
         X1 = np.asarray(X1)
         X2 = np.asarray(X2)
+        cen = (X1+X2)/2
+        cen = cen.astype(int)
         nor = np.sqrt((X2[0]-X1[0])**2+(X2[1]-X1[1])**2)
         u1 = (X2-X1)/nor
+        u2 = np.asarray([-u1[1],u1[0]])
         m = 255
         for i in range(10,int(nor)-10):
             x = int(X1[0]+u1[0]*(i+.5))
@@ -46,9 +49,20 @@ def draw_eye(X1,X2, gray,img):
                 m = m1
                 cx = x
                 cy = y
+        X0 = np.asarray([cx,cy])
+        for i in range(-int(nor/4),int(nor/4)):
+            x = int(X0[0]+u2[0]*(i+.5))
+            y = int(X0[1]+u2[1]*(i+.5))
+            #m1 = np.mean(gray[(x-1):(x+1),(y-1):(y+1)])
+            m1 = gray[y,x]
+            if m1<m:
+                m = m1
+                cx = x
+                cy = y
         cv2.circle(img, (cx, cy), 4, (0, 0, 255), 2)
-        rat = np.abs(cx-X2[0])/nor
-        return rat
+        #cv2.circle(img, (cen[0],cen[1]), 4, (255, 0, 0), 2)
+        coor = 2*(np.asarray([cx,cy])-cen)/nor
+        return [-coor[0],coor[1]]
     except:
         pass
 
@@ -57,6 +71,8 @@ predictor = dlib.shape_predictor('shape_68.dat')
 
 left = [36, 37, 38, 39, 40, 41]
 right = [42, 43, 44, 45, 46, 47]
+
+rat = 2000
 
 cap = cv2.VideoCapture(0)
 ret, img = cap.read()
@@ -86,7 +102,8 @@ while(True):
         eyes[mask] = [255, 255, 255]
         mid = (shape[42][0] + shape[39][0]) // 2
         eyes_gray = cv2.cvtColor(eyes, cv2.COLOR_BGR2GRAY)
-        threshold = cv2.getTrackbarPos('threshold', 'image')
+        #threshold = cv2.getTrackbarPos('threshold', 'image')
+        threshold = 200
         _, thresh = cv2.threshold(eyes_gray, threshold, 255, cv2.THRESH_BINARY)
         thresh = cv2.erode(thresh, None, iterations=2) #1
         thresh = cv2.dilate(thresh, None, iterations=4) #2
@@ -99,15 +116,15 @@ while(True):
             a4 = np.argmin(a[1])
             b2 = np.argmax(b[1])
             b4 = np.argmin(b[1])
-            lr = draw_eye( (a[1][a4], a[0][a4]),(a[1][a2], a[0][a2]), gray,img)
-            rr = draw_eye( (mid+b[1][b4], b[0][b4]), (mid+b[1][b2], b[0][b2]),gray,img)
-            cv2.line(img, (a[1][a2], a[0][a2]), (a[1][a4], a[0][a4]), (0, 255, 0), 1)
-            cv2.line(img, (mid+b[1][b2], b[0][b2]), (mid+b[1][b4], b[0][b4]), (0, 255, 0), 1)
-            cv2.line(img, (int(4*(lr+rr)*mid-3*mid), 0), (int(4*(lr+rr)*mid-3*mid), thresh.shape[0]), (0, 255, 0), 1)
+            lp = draw_eye( (a[1][a4], a[0][a4]),(a[1][a2], a[0][a2]), gray,img)
+            rp = draw_eye( (mid+b[1][b4], b[0][b4]), (mid+b[1][b2], b[0][b2]),gray,img)
+         #   cv2.line(img, (a[1][a2], a[0][a2]), (a[1][a4], a[0][a4]), (0, 255, 0), 1)
+         #   cv2.line(img, (mid+b[1][b2], b[0][b2]), (mid+b[1][b4], b[0][b4]), (0, 255, 0), 1)
+            cv2.circle(img, (int((lp[0]+rp[0])/2*rat+mid), int((lp[1]+rp[1])/2*rat+200)), 2, (0, 0, 0), 2)
         except:
             pass
     cv2.imshow('eyes', img)
-    cv2.imshow("image", thresh)
+   # cv2.imshow("image", thresh)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
