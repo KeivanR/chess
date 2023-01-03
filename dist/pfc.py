34e30c0129064bcbfbc569d4ce84f3f2feada76a
Keivan RAZBAN <34062925+KeivanR@users.noreply.github.com>
2023-01-03 14:22:32 +0000

    Update pfc.py

--- dist/pfc.py
+++ dist/pfc.py
@@ -5,7 +5,7 @@ import cv2
 
 
 class Group:
-    def __init__(self, width, height, list_x, list_y, list_values, list_theta, list_mag, r_max, size):
+    def __init__(self, width, height, list_x, list_y, list_values, list_theta, list_mag, r_max, size, speeds):
         self.width = width
         self.height = height
         self.N = len(list_x)
@@ -15,6 +15,7 @@ class Group:
         self.r_max = r_max
         self.scale = 8
         self.size = size
+        self.speeds = speeds
         k = 0
         for x, y, val, theta, mag in zip(list_x, list_y, list_values, list_theta, list_mag):
             self.things.append(Thing(x, y, val, theta, mag, size))
@@ -46,6 +47,44 @@ class Group:
             r += 1
         return found, int(close - 1), touch
 
+    def update_by_rank(self, rank):
+        for i in range(self.N-1):
+            yref0 = self.things[rank[i]].y
+            yref1 = self.things[rank[i + 1]].y
+            xref0 = self.things[rank[i]].x
+            xref1 = self.things[rank[i + 1]].x
+            around = (xref1 - xref0)**2+(yref1-yref0)**2 < self.r_max**2
+            touch = (xref1 - xref0)**2+(yref1-yref0)**2 < self.size**2
+            if around:
+                if touch and (self.things[rank[i]].value - self.things[rank[i + 1]].value) % 3 == 1:
+                    self.things[rank[i]].value = self.things[rank[i + 1]].value
+                elif touch and (self.things[rank[i]].value - self.things[rank[i + 1]].value) % 3 == 2:
+                    self.things[rank[i+1]].value = self.things[rank[i]].value
+                elif self.things[rank[i]].value != self.things[rank[i + 1]].value:
+                    self.things[rank[i]].mag = 1 + (self.things[rank[i]].value - self.things[rank[i + 1]].value) % 3
+                    self.things[rank[i]].theta = math.atan2(yref1 - yref0, xref1 - xref0)
+                    if self.things[rank[i]].mag == 2:
+                        self.things[rank[i]].theta += 3.1415
+
+                    self.things[rank[i+1]].mag = 1 + (self.things[rank[i+1]].value - self.things[rank[i]].value) % 3
+                    self.things[rank[i+1]].theta = math.atan2(yref1 - yref0, xref1 - xref0)
+                    if self.things[rank[i+1]].mag == 1:
+                        self.things[rank[i+1]].theta += 3.1415
+            else:
+                self.things[rank[i]].mag = 1
+
+    def update_speeds2(self):
+        list_x = []
+        for i in range(self.N):
+            list_x.append(self.things[i].x)
+        rank = np.argsort(np.array(list_x))
+        self.update_by_rank(rank)
+        list_y = []
+        for i in range(self.N):
+            list_y.append(self.things[i].y)
+        rank = np.argsort(np.array(list_y))
+        self.update_by_rank(rank)
+
     def update_speeds(self):
         visited = []
         for i in range(self.N):
@@ -68,8 +107,8 @@ class Group:
 
     def update_pos(self):
         for i in range(self.N):
-            self.things[i].x += self.scale * self.things[i].mag * np.cos(self.things[i].theta)
-            self.things[i].y += self.scale * self.things[i].mag * np.sin(self.things[i].theta)
+            self.things[i].x += self.scale * self.speeds[int(self.things[i].mag-1)] * np.cos(self.things[i].theta)
+            self.things[i].y += self.scale * self.speeds[int(self.things[i].mag-1)] * np.sin(self.things[i].theta)
             margin = 20
             if self.things[i].x < self.r_max + margin + self.things[i].size:
                 if np.pi/2<(self.things[i].theta%(2*np.pi))<3*np.pi/2:
@@ -86,15 +125,16 @@ class Group:
         self.grid = np.zeros((width + 2 * r_max, height + 2 * r_max))
         self.grid_val = np.zeros((width, height))
         for i in range(self.N):
-            self.grid[int(self.things[i].x), int(self.things[i].y)] = i + 1
+            self.grid[(int(self.things[i].x))%self.width, (int(self.things[i].y))%self.width] = i + 1
             for a in range(-self.things[i].size, self.things[i].size):
                 for b in range(-self.things[i].size, self.things[i].size):
                     if self.things[i].value == 1 and self.things[i].size**2*4/9<(a**2 + b**2) < self.things[i].size**2-3:
                         self.grid_val[(int(self.things[i].x) - self.r_max + a)%self.width, (int(self.things[i].y) - self.r_max + b)%self.height] = 255
                     if self.things[i].value == 0 and -self.things[i].size+0<b< -0+self.things[i].size:
                         self.grid_val[(int(self.things[i].x) - self.r_max + a)%self.width, (int(self.things[i].y) - self.r_max + b)%self.height] = 255
-                    if self.things[i].value == 2 and (a==b or a==-b or a==0 and b<0 or b<1-self.things[i].size):
-                        self.grid_val[(int(self.things[i].x) - self.r_max + a)%self.width, (int(self.things[i].y) - self.r_max + b)%self.height] = 255
+                    if self.things[i].value == 2 and (np.abs(b)-1<=np.abs(a)<=np.abs(b)+1 or a==0 and b<0 or b<1-self.things[i].size):
+                        self.grid_val[(int(self.things[i].x) - self.r_max + a)%self.width, (int(self.things[i].y - self.r_max + 1.6*b))%self.height] = 255
+
 
     def display(self):
         cv2.imshow('Frame', self.grid_val)
@@ -115,9 +155,10 @@ class Thing:
 
 width = 600
 height = 900
-N = 20
-r_max = 25
-size = 10
+N = 50
+r_max = 100
+size = 8
+speeds = [1,1,3]
 
 list_x = np.random.random(N) * (width-size) + r_max + size
 list_y = np.random.random(N) * (height-size) + r_max + size
@@ -125,7 +166,7 @@ list_theta = np.random.random(N) * 3.1415
 list_mag = np.random.random(N) * 0.5
 list_values = (np.arange(N) / N * 3).astype(int)
 print(list_values)
-things = Group(width, height, list_x, list_y, list_values, list_theta, list_mag, r_max, size)
+things = Group(width, height, list_x, list_y, list_values, list_theta, list_mag, r_max, size,speeds)
 
 while 1:
     #things.display()
@@ -133,5 +174,5 @@ while 1:
     # Press Q on keyboard to  exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
-    things.update_speeds()
+    things.update_speeds2()
     things.update_pos()
